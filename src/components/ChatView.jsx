0a1eff2a59a2c782d7b6a39a81d6f8ecf8b5b190
Owen <ocolegro@gmail.com>
2023-11-02 00:46:33 -0400

    disable when sending

--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -2,7 +2,6 @@ import { useState, useRef, useEffect, useContext } from "react";
 import Message from "./Message";
 import { ChatContext } from "../context/chatContext";
 import Thinking from "./Thinking";
-import { replaceProfanities } from "no-profanity";
 import ReactDOM from "react-dom";
 import {
   AppBar,
@@ -66,6 +65,7 @@ const ChatView = () => {
   const [modalContext, setModalContext] = useState(null);
   const [visibleContext, setVisibleContext] = useState(false);
   const [context, setContext] = useState([]);
+  const [sendingMessage, setSendingMessage] = useState(false);
 
   const initialMessageProcessed = useRef(false);
   const [
@@ -115,7 +115,6 @@ const ChatView = () => {
    */
   const sendMessage = async (e, initialMessage = null) => {
     e?.preventDefault(); // e will be undefined when called programmatically
-
     const messageToSend = initialMessage || formValue;
     const cleanPrompt = messageToSend; //replaceProfanities(messageToSend);
 
@@ -241,35 +240,6 @@ const ChatView = () => {
   useEffect(() => {
     inputRef.current.focus();
   }, []);
-  // const ContextItem = [
-  //   [
-  //     {
-  //       id: 12333661,
-  //       title: "Hine E Hine",
-  //       text: 'Hine E Hine "" is a Māori lullaby written by Princess Te Rangi Pai (Fanny Rose Howie née Porter) (1868–1916) in 1907. An instrumental version of "" was used from 1979 to 1994 as the New Zealand TV Channel 2\'s "closing-down song", which accompanied a cartoon featuring the Goodnight Kiwi. It was the opening song on Kiri Te Kanawa\'s 1999 album "Maori Songs". Hayley Westenra sang the song on her 2003 album "Pure". The song features on the José Carreras album "The José Carreras Collection". The Phoenix Foundation performed the song in the 2010 film "Boy". Alternative words were supplied',
-  //     },
-  //     {
-  //       id: 20753222,
-  //       title: "Myrtle K. Hilo",
-  //       text: "Myrtle K. Hilo Myrtle K. Hilo (May 17, 1929 - October 3, 2009) was a native Hawaiian taxicab driver, radio personality, ukulele player and singer. Her signature album \"The Singing Cab Driver\" was released in 1967 on Makaha Records. She was born in Hauʻula, Hawaii on the island of O'ahu. In 1998 she received the Lifetime Achievement Award from the Hawai'i Academy of Recording Arts. Myrtle Kahea Hilo was born of Hawaiian ancestry in Hauʻula on the island of O'ahu, in the Territory of Hawaii. In 1948, she married heavy equipment operator George Hilo. They became the parents of one",
-  //     },
-  //     {
-  //       id: 7799829,
-  //       title: "Hi (kana)",
-  //       text: 'Hi (kana) ひ, in hiragana, or ヒ in katakana, is one of the Japanese kana, which each represent one mora. Both can be written in two strokes, sometimes one for hiragana, and both are phonemically although for phonological reasons, the actual pronunciation is , the sound would be nearer to be transcribed "hyi" in ro-maji. The pronunciation of the voiceless palatal fricative [ç] is similar to that of the English word hue [çuː] for some speakers. In the Sakhalin dialect of the Ainu language, ヒ can be written as small ㇶ to represent a final h sound after an "i"',
-  //     },
-  //     {
-  //       id: 936417,
-  //       title: "Hina (goddess)",
-  //       text: 'book, "The Legend of Te Tuna". Also, in his popular book "The Seven Daughters of Eve", Bryan Sykes used Hina\'s name, (spelled therein "Ina") to denote the clan matriarch of mtDNA haplogroup B. David Lee Roth recorded a song called "Hina", contained on the hard rock album "Skyscraper", released in 1988. Hina (goddess) Hina is the Eastern Polynesian variant for the given name Sina. Hina/Sina is the name assigned to a number of Polynesian goddesses and queens. Among the Iwi of New Zealand, Hina is usually considered to be either the elder sister or the wife of Maui. The most',
-  //     },
-  //     {
-  //       id: 936475,
-  //       title: "Hiʻiaka",
-  //       text: 'Hiʻiaka In Hawaiian mythology, Hiʻiaka is a daughter of Haumea and Kāne. Hiiaka, or the youngest Hiiaka, was the patron goddess of Hawaii, hula dancers, chant, sorcery and medicine. Owls were her messengers and were sacred to her. Her common and family name means "carried egg" - "hii", to hold or carry in the arms (as a child), and "aka", meaning embryo - referring to the story of how she was brought to Hawaii by her sister Pele. Her family line is called Hiiaka, and they take on the task of bearing the clouds, providing rain, thunder and lightning variously,',
-  //     },
-  //   ],
-  // ];
 
   return (
     <>
@@ -389,6 +359,7 @@ const ChatView = () => {
         >
           <form
             onSubmit={sendMessage}
+            disabled={thinking}
             style={{
               display: "flex",
               justifyContent: "space-between",
